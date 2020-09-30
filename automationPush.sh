905e7a751549d04da13af3eefd8294b7125dcf6e
Jordan Bonaldi <jordanbonaldi@me.com>
2020-09-30 14:25:07 +0200

    gitlab syncé

--- automationPush.sh
+++ automationPush.sh
@@ -1,5 +1,5 @@
 mkdir .indepProject && cd .indepProject &&
-git clone git@github.com:jordanbonaldi/WebViewsInjector.git &&
+git clone https://gitlab.com/privelt-uk/WebViewsInjector.git &&
 cd WebViewsInjector &&
 cp ../../$1Service.java ./webviewsinjector/src/main/java/net/neferett/webviewsinjector/services/. &&
 git checkout -b impl_$1 &&
